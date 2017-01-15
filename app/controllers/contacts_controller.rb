ce93db7fe6d7478909b2feae56e6a2a47695e0da
Sean Ruffatti <seanruffatti@yahoo.com>
2017-01-15 20:02:05 +0000

    added comments for contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,14 @@
 class ContactsController < ApplicationController
+  
+  #GET request to /contact-us
+  #Show new contact form
   def new
     @contact = Contact.new
   end
   
+  #POST request /contacts
   def create
+    #Mass assignment of form fields to Contact object
     @contact = Contact.new(contact_params)
       if @contact.save
         #Lifting from contact form ([:contact]) and grabbing name, email, and comments. Then plugging
@@ -12,7 +17,8 @@ class ContactsController < ApplicationController
         email = params[:contact][:email]
         body = params[:contact][:comments]
         ContactMailer.contact_email(name, email, body).deliver
-        #
+        #Flash notice for successful save to database. 
+        #Success comes from bootstrap. Flash method is in application.html.erb
         flash[:success] = "Message sent."
         redirect_to new_contact_path
         
@@ -22,6 +28,8 @@ class ContactsController < ApplicationController
       end
   end
   
+  #To collect data from form, we need to use
+  # strong parameters and whitelist the form fields
   private
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
